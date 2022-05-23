470d8dccff758d8a9549d312d7aa2895e7bffd03
he <bgyhenrietta@yahoo.com>
2022-05-23 10:52:17 +0200

    Konfiguráció Javaban - visszaállítás

--- spring-solutions/locations/src/main/java/locations/LocationsService.java
+++ spring-solutions/locations/src/main/java/locations/LocationsService.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//@Service
+@Service
 public class LocationsService {
 
     private List<Location> locations = Arrays.asList(
