4ecb9b790717c46845fd46ccfcfe946f8d4143d7
he <bgyhenrietta@yahoo.com>
2022-05-23 05:06:15 +0200

    Spring Beanek

--- spring-solutions/locations/src/main/java/locations/LocationsController.java
+++ spring-solutions/locations/src/main/java/locations/LocationsController.java
@@ -3,19 +3,17 @@ package locations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 public class LocationsController {
 
-    private List<Location> locations = Arrays.asList(
-            new Location("First Location", 25.142536, 36.748596),
-            new Location("Second Location", 47.142536, 45.635241)
-    );
+    private LocationsService locationsService;
+
+    public LocationsController(LocationsService locationsService) {
+        this.locationsService = locationsService;
+    }
 
     @GetMapping("/")      // ("/locations")
     public String getLocations() {
-        return locations.toString();
+        return locationsService.getLocations().toString();
     }
 }
---
+++ spring-solutions/locations/src/main/java/locations/LocationsService.java
@@ -0,0 +1,20 @@
+package locations;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class LocationsService {
+
+    private List<Location> locations = Arrays.asList(
+            new Location("First Location", 35.142536, 36.748596),
+            new Location("Second Location", 37.142536, 45.635241)
+    );
+
+    public List<Location> getLocations() {
+        return new ArrayList<>(locations);
+    }
+}
